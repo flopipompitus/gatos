65133342c4711ed80cb8512effc216cdbc984fd3
flopipompitus <florencia.lagos@estudiantes.colegiotgs.cl>
2025-10-06 14:48:51 -0300

    Add files via upload

---
+++ pages/gatos99.py
@@ -0,0 +1,16 @@
+# Importar streamlit
+import streamlit as st
+
+# ---------------------  Contenido del sitio   --------------------------#
+# Sección Inicial
+st.title("gatitos tiernos")
+st.badge("gato", color="green", icon=":material/chevron_right:")
+
+# Sutítulo
+st.header("", divider=True)
+#imagen
+st.image("gato3.jpg", caption="lindo gatito")
+#escitura
+st.write("""
+
+gatitos lindos gatitos yo amo los gatitos""")
