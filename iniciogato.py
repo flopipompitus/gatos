57d83992097060dc7f16405fd3c30a4ce32ba0b1
flopipompitus <florencia.lagos@estudiantes.colegiotgs.cl>
2025-10-06 14:35:50 -0300

    Update iniciogato.py

--- iniciogato.py
+++ iniciogato.py
@@ -13,5 +13,12 @@ st.logo(
     "logogato1.jpeg",
 )
 #para iniciar las paginas
-pg = st.navigation(["homepage.py", "gatos99.py" , "mantequilla.py" , "paris.py" , "quienes.py"])
-pg.run()
+#pg = st.navigation(["homepage.py", "gatos99.py" , "mantequilla.py" , "paris.py" , "quienes.py"])
+#pg.run()
+
+    # Links de navegación
+    st.page_link("home.py", label="Inicio", icon=":material/home:")
+    st.page_link("pages/1_productos.py", label="Productos", icon=":material/density_small:")
+    st.page_link("pages/2_servicios.py", label="Servicios", icon=":material/assistant_navigation:")
+    st.page_link("pages/3_nosotros.py", label="Nosotros", icon=":material/group_search:")
+    st.page_link("pages/4_contacto.py", label="Contacto", icon=":material/mobile:")
